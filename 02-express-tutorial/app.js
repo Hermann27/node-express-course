c860b8690ec04f10e8966b80ea85b8563fd43e39
hdjoumdjeu <hermannpougoue1@gmail.com>
2025-04-05 22:59:41 -0500

    Implemented Express setup, static file hosting, JSON APIs, and query-based product retrieval.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,67 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+
+const { products } = require("./data.js");
+app.use(express.static("./public"));
+
+app.get("/api/v1/test", (req, res) => {
+  res.json({ message: "It worked!" });
+});
+
+app.get("/api/v1/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/v1/products/:productID", (req, res) => {
+  const idToFind = parseInt(req.params.productID);
+  const product = products.find((p) => p.id === idToFind);
+  if (!product) {
+    return res.status(404).json({ message: "That product was not found." });
+  }
+  res.json(product);
+});
+
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit, maxPrice, regex } = req.query;
+  let sortedProducts = [...products];
+
+  // Filter by starting letters
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) =>
+      product.name.startsWith(search)
+    );
+  }
+  // Limit the number of results
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, parseInt(limit));
+  }
+  // Filter by max price
+  if (maxPrice) {
+    sortedProducts = sortedProducts.filter(
+      (product) => product.price <= parseFloat(maxPrice)
+    );
+  }
+  /* 
+    Filter by regular expression
+    'i' for case-insensitive matching  
+  */
+  if (regex) {
+    const regexPattern = new RegExp(regex, "i");
+    sortedProducts = sortedProducts.filter((product) =>
+      regexPattern.test(product.name)
+    );
+  }
+  // If no products found, return an empty array
+  if (sortedProducts.length < 1) {
+    return res.status(200).json({ success: true, data: [] });
+  }
+  // If products found, return the sorted array
+  res.status(200).json(sortedProducts);
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
+app.listen(3000, () => {
+  console.log("server is listening on port 3000...");
+});
